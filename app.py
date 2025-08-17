f6a36e8171210dc711ae4af771b52c211a0ea540
Gustavo Barrios <gabarrios2006@gmail.com>
2025-08-16 23:55:07 -0300

    Criando as mensagens de abertura do programa e exibição do menu.

---
+++ app.py
@@ -0,0 +1,6 @@
+print('ˢᵃᵇᵒʳ ᴱˣᵖʳᵉˢˢ\n')
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+opcao_escolhida = input('Escolha uma opção: ')
